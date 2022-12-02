c5223e599553b1441c0a0b0a4f747eee9d190482
Mohamed Aiad <mohamedaiad2000@gmail.com>
2022-12-02 22:02:06 +0200

    updated loginService file and added new class DButil package

--- sprint1/src/main/java/com/school/sprint1/Service/loginService.java
+++ sprint1/src/main/java/com/school/sprint1/Service/loginService.java
@@ -1,22 +1,11 @@
 package com.school.sprint1.Service;
 
-import com.school.sprint1.DButil.DButil;
+import com.school.sprint1.DButil.PersonDB;
 import org.springframework.stereotype.Service;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Objects;
 
 @Service
 public class loginService {
-
-    private Connection connection;
-
-    public loginService() {
-        connection = DButil.getConnection();
-    }
 
     public boolean run(String input){
         String[] input_split = split(input);
@@ -25,22 +14,14 @@ public class loginService {
         return check(id,pass);
     }
 
-    public String[] split(String input){
+    private String[] split(String input){
         String[] input_split = input.split(",");
         return input_split;
     }
 
-    public boolean check(String id,String pass){
-        String passFromDb = "";
-        try {
-            PreparedStatement statement = connection.prepareStatement("select password from person where id = " + id);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()){
-                passFromDb = resultSet.getString(1);
-            }
-        } catch (SQLException e) {
-            return false;
-        }
+    private boolean check(String id,String pass){
+        PersonDB personDB = new PersonDB();
+        String passFromDb = personDB.getPass(id);
         if(Objects.equals(pass, passFromDb)) return true;
         return false;
     }
