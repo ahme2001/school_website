b891871bdc44b22bd8d42b57f0841078e6a785ee
Mohamed Aiad <mohamedaiad2000@gmail.com>
2022-12-17 04:20:39 +0200

    Exam table implemented

---
+++ sprint1/src/main/java/com/school/sprint1/gson/Day_Lecture.java
@@ -0,0 +1,22 @@
+package com.school.sprint1.gson;
+
+public class Day_Lecture {
+    private String Day;
+    private Lectures lectures;
+
+    public String getDay() {
+        return Day;
+    }
+
+    public void setDay(String day) {
+        this.Day = day;
+    }
+
+    public Lectures getLectures() {
+        return lectures;
+    }
+
+    public void setLectures(Lectures lectures) {
+        this.lectures = lectures;
+    }
+}
---
+++ sprint1/src/test/java/com/school/sprint1/testSchedule.java
@@ -0,0 +1,7 @@
+package com.school.sprint1;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class testSchedule {
+}
